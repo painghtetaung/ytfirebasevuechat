252dd1f8847de3869fa925d7b84c6ec6f2b46d11
painghtetaung <painghtetaung1999@gmail.com>
2021-07-07 14:58:02 +0630

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase'
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBVD1nbPcZOV1lpZ79utuaeEHLAbK700EY",
+    authDomain: "vue-firestore-chat-51559.firebaseapp.com",
+    projectId: "vue-firestore-chat-51559",
+    storageBucket: "vue-firestore-chat-51559.appspot.com",
+    messagingSenderId: "591199542004",
+    appId: "1:591199542004:web:e59aa403be8eed575a5ca1",
+    measurementId: "G-9V9CWRWB29"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 createApp(App).use(store).use(router).mount('#app')
